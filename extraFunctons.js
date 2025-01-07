ab4a4818ea803bf5853539021e137fbaa89504d0
playblackjack <dstyjgtyuguodcyuj@outlook.com>
2025-01-07 10:24:03 -0500

    Update extraFunctons.js

--- extraFunctons.js
+++ extraFunctons.js
@@ -1,12 +1,1 @@
-let game;
-
-function startGame(role) {
-    document.getElementById('role-select').style.display = 'none';
-    document.getElementById('game-table').style.display = 'block';
-    game = new BlackjackGame();
-    game.playerRole = role;
-}
-
-function addToBet(amount) {
-    game.addToBet(amount);
-}
+let game; function startGame(role) { document.getElementById('role-select').style.display = 'none'; document.getElementById('game-table').style.display = 'block'; game = new BlackjackGame(); game.playerRole = role; } function addToBet(amount) { game.addToBet(amount); }
