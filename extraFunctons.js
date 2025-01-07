0c6a36ab58279ee5da85898e8e8dfb14e7d2c21b
playblackjack <dstyjgtyuguodcyuj@outlook.com>
2025-01-07 12:01:07 -0500

    Update extraFunctons.js

--- extraFunctons.js
+++ extraFunctons.js
@@ -1,1 +1,12 @@
-letgame;functionstartGame(role){document.getElementById('role-select').style.display='none';document.getElementById('game-table').style.display='block';game=newBlackjackGame();game.playerRole=role;}functionaddToBet(amount){game.addToBet(amount);}
+letgame;
+
+functionstartGame(role) {
+    document.getElementById('role-select').style.display = 'none';
+    document.getElementById('game-table').style.display = 'block';
+    game = newBlackjackGame();
+    game.playerRole = role;
+}
+
+functionaddToBet(amount) {
+    game.addToBet(amount);
+}
